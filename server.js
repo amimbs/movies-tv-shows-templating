3e0f0f97d1d36973527a154d585fa716072ba179
amimbs <andymimbs91@gmail.com>
2022-03-22 22:39:21 -0400

    Added watch_list.ejs, two new get requests for movies and series. Their pages render correctly

--- server.js
+++ server.js
@@ -13,13 +13,28 @@ app.get('/api', (req, res) => {
     res.render('index');
 });
 
-app.get('/api/media_list', (req, res) => {
-    res.render('media_list');
-})
+app.get('/api/watch_list', (req, res) => {
+    res.render('watch_list');
+});
 
-// app.get('/api/media/:Title', (req, res) => {
-//     let foundMedia = mediaList.filter((media) => media.Title == req.params.Title);
-//     res.render('media_list', { displayMedia: foundMedia });
+// render titles by type "movie"
+app.get('/api/movies', (req, res) => {
+    let movie = mediaList.filter((titles) => titles.Type =='movie');
+    // console.log(movie);
+    res.render('movies', { titles: movie });
+});
+
+// render titles by type "series"
+app.get('/api/series', (req, res) => {
+    let show = mediaList.filter((titles) => titles.Type =='series');
+    console.log(show);
+    res.render('series', { titles: show });
+});
+
+// This one selects all media by their titles
+// app.get('/api/media_list', (req, res) => {
+//     // route level key value pair
+//     res.render('media_list', { media_list: mediaList });
 // });
 
 
